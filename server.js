cf1321d5a0a195e53f832060633c9f3524208244
LUISOCE14 <luis.ocegueda8927@alumnos.udg.mx>
2022-03-29 15:56:48 -0600

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/myapp'));
+app.use(express.static(__dirname + '/dist/aplicacionAngular'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/aplicacionAngular/index.html'));});
 app.listen(process.env.PORT || 8080);
